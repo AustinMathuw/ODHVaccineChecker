12e4d977d94db8bb0f51206587a764b54bf52115
Austin Mathuw <siraustin1234@gmail.com>
2021-03-30 01:35:59 -0400

    update

--- system/run.sh
+++ system/run.sh
@@ -1,3 +1,30 @@
-git fetch
-git pull
-cd bot && npx nightwatch vaccine_check.js
+#!/bin/bash
+
+export searchLocation="2000 PROSPECT AVE E CLEVELAND OH 44115";
+export vaccineLocation="Wolstein Center"
+export snsArn="arn:aws:sns:us-east-1:731744095435:VaccineBot";
+
+unix="$(date +%s)"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ -d "$DIR/logs" ] 
+then
+	rm -r $DIR/logs
+fi
+
+mkdir -p $DIR/logs
+logFile="$DIR/logs/$unix.log"
+
+main() {
+	git fetch
+	git pull
+	cd bot && npx nightwatch vaccine_check.js
+}
+
+
+main 2>&1 | (
+    # The INT trap makes sure that logging continues when vetguardian ends in a fault
+    # See https://unix.stackexchange.com/questions/407233/how-to-pipe-a-bash-command-and-keep-ctrlc-working
+    trap '' INT
+    tee $logFile
+)
